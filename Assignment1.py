90b8ef2d1933c7e26235c6d2c8cfb881b2ecbc1b
AllenDevaraj <86686418+AllenDevaraj@users.noreply.github.com>
2021-07-03 00:55:59 +0530

    Assignment-1, 2/7/2021 done.

---
+++ Assignment1.py
@@ -0,0 +1,14 @@
+#Task 1: To write a program that calculates the area of circle
+r = float(input('Input the radius of circle: '))
+def area(r):
+  print('The area of the circle with radius ',r,' is ',3.14*r*r)
+area(r)
+
+#Task 2: To display the extension of given file
+f = input('Input the Filename: ')
+if s[-3]=='.' and s[-2]=='p' and s[-1]=='y':
+  print("The extension of the file is : 'python'")
+else:
+  print("The extension of the file is not 'python'")
+  
+#Thank You
