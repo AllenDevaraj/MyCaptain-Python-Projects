ef7ad18de4c996c9f3caba14935c5dad91651dfa
AllenDevaraj <86686418+AllenDevaraj@users.noreply.github.com>
2021-07-05 12:14:57 +0530

    Assignment2Task1

---
+++ Assignment2Task1.py
@@ -0,0 +1,11 @@
+#Task 1: To display fibonacci series for n numbers
+n = int(input('Enter no. of numbers you would like to see from fibonacci series: '))
+first = 0
+second = 1
+print(first)
+print(second)
+for i in range(n):
+  third = first + second
+  print(third)
+  first = second
+  second = third
