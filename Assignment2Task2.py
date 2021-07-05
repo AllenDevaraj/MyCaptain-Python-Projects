cd41983974e6f49f4ee5201cf68e1ddc324f44c4
AllenDevaraj <86686418+AllenDevaraj@users.noreply.github.com>
2021-07-05 12:20:21 +0530

    Assignment2Task2

---
+++ Assignment2Task2.py
@@ -0,0 +1,9 @@
+#Task 2: To display positive no. from a range
+list = eval(input('Enter list of numbers: '))
+ans = []
+for i in list:
+  if i > 0:
+    ans.append(i)
+print('Output: ',ans)
+
+#Thank You
